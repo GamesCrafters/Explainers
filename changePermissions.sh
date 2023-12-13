dd76bdcd08509a1e62aa56f6ee2bfb8baa16ca21
Cameron Cheung <cameronlcheung@yahoo.com>
2023-12-12 18:05:15 -0800

    Add Change Permissions Shell Script

---
+++ changePermissions.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Changing Permissions"
+find instructions -type d -exec chmod 755 {} \;
+find instructions -type f -exec chmod 644 {} \;
+find site-refresh-sp20 -type d -exec chmod 755 {} \;
+find site-refresh-sp20 -type f -exec chmod 644 {} \;
